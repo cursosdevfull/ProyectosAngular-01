7a82b51d504cb1738413bf6cb67655eb1fc8560a
sergio <info@cursos-dev.com>
2020-02-02 12:07:51 -0500

    comunicación bidireccional

--- Clase01/src/app/registro/registro.component.ts
+++ Clase01/src/app/registro/registro.component.ts
@@ -3,13 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
-  styleUrls: ['./registro.component.css']
+  styleUrls: ['./registro.component.css'],
 })
 export class RegistroComponent implements OnInit {
+  titulo = 'Registro';
+  nombre = 'Sergio';
+  correo = 'sergiohidalgocaceres@gmail.com';
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    setTimeout(() => {
+      this.titulo = 'Registro de Participante';
+    }, 3000);
   }
 
+  tipeo(evt) {
+    this.correo = evt.target.value;
+    /* console.log(evt.target.value); */
+  }
+
+  ngOnInit() {}
 }
